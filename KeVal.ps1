1c2b46771a4c25c207dfa705b11df6227a3d1bf1
Oded Rafi <Odedrafi74@gmail.com>
2022-03-03 21:13:05 +0200

    making a powershell-feature

---
+++ KeVal.ps1
@@ -0,0 +1,51 @@
+
+
+ [string]$password=$args[0]#waiting for password from user 
+#  write-host $key
+
+ function PasswordValidator(){
+   ##init flags for error massege
+    $ErrorString1=$null
+    $ErrorString2=$null
+    $ErrorString3=$null
+
+    if ( ( $password -notmatch '\d') -eq $true ){#checking for flag 3
+
+        [string]$ErrorString3="[0-9]"
+
+    }
+    if ( ( $password -cmatch '[A-Z]') -ne $true){#checking for flag 2
+
+        [string]$ErrorString2="[A-Z]"
+
+    }
+    if ( ( $password -cmatch '[a-z]') -ne $true){#checking for flag 1
+
+            [string]$ErrorString1="[a-z]"
+
+        } 
+
+
+        #if all the flags are down valid password(checking if flags are null or empty)
+        if( [string]::IsNullOrEmpty($ErrorString1) -and  [string]::IsNullOrEmpty($ErrorString2) -and [string]::IsNullOrEmpty($ErrorString3)){
+
+            Write-host "Password Valid"-ForegroundColor Green
+
+            exit 0
+        
+        }
+        #else print to screen error message according to flags
+        else{
+
+            Write-host "Password need's to containe {$ErrorString1$ErrorString2$ErrorString3}" -ForegroundColor Red
+            exit 1
+
+        }
+
+
+}
+
+
+
+
+   PasswordValidator($key)
