f593276d6c836b0b2e4d58d22599419b2c68ca9a
Oded Rafi <Odedrafi74@gmail.com>
2022-03-03 20:45:51 +0200

    changing Function to look less messy

--- Script.sh
+++ Script.sh
@@ -1,64 +1,60 @@
 #!/usr/bin/bash
 
-#A function to validate a password by four parameters
+##define color var##
+RED="\e[31m"
+GREEN="\e[32m"
+ENDCOLOR="\e[0m"
+
+PASSWORD="${1}"
+
+if [[ ${#PASSWORD} -lt 10 ]]; then #First checking string len les than 10 return an error
+  echo -e "${RED}Password is too short${ENDCOLOR}"
+  exit 1
+fi
+
 PasswordValidator() {
 
-  ##define color var##
-  RED="\e[31m"
-  GREEN="\e[32m"
-  ENDCOLOR="\e[0m"
+  ##init flags for error massege
+  ErrorFlag1="$null"
+  ErrorFlag2="$null"
+  ErrorFlag3="$null"
 
-  PASSWORD="${1}"
+  ##init conditions
+  condition1="[a-z]"
+  condition2="[A-Z]"
+  condition3="[0-9]"
 
-  Condition1=[a-z] # init the condition
-  Condition2=[A-Z]
-  Condition3=[0-9]
+  if [[ ! $PASSWORD =~ $condition1 ]]; then #checking for flag 3
 
-  if [[ ${#PASSWORD} -lt 10 ]]; then #First checking string len les than 10 return an error
-    echo -e "${RED}Password is too short${ENDCOLOR}"
+    ErrorFlag1="[a-z]"
+
+  fi
+  if [[ ! $PASSWORD =~ $condition2 ]]; then #checking for flag 2
+
+    ErrorFlag2="[A-Z]"
+
+  fi
+  if [[ ! $PASSWORD =~ $condition3 ]]; then
+
+    ErrorFlag3="[0-9]"
+
+  fi
+
+  #if all the flags are down valid password(checking if flags are null or empty)
+  if [[ ("$ErrorFlag1" == $null) && ("$ErrorFlag2" == $null) && ("$ErrorFlag3" == $null) ]]; then
+
+    echo -e "${GREEN}Password is valid${ENDCOLOR}"
+
+    exit 0
+
+  #else print to screen error message according to flags
+  else
+
+    echo -e "${RED}Password need's to containe {$ErrorFlag1$ErrorFlag2$ErrorFlag3}${ENDCOLOR}"
     exit 1
+
   fi
 
-  # going over all the condition permutations inorder to present the wanted error message
-
-  if [[ ! $PASSWORD =~ $Condition1 ]]; then # if password dosn't answer 1 then chack next
-
-    if [[ ! $PASSWORD =~ $Condition2 ]]; then   # and password dosn't answer 2 then chack next conditions
-      if [[ ! $PASSWORD =~ $Condition3 ]]; then # and password dosn't answer 3 then exit with the propper error
-        echo -e "${RED}Password need's to containe "{$Condition1+ $Condition2+ $Condition3}" ${ENDCOLOR}"
-        exit 1
-      else
-        echo -e "${RED}Password need's to containe "{$Condition1+$Condition2}"${ENDCOLOR} " # when only 3 is ok then an error on the other 2 is returnd
-        exit 1
-      fi
-    else                                                                        # when 1 is missing 2 is ok we check to see about  3
-      if [[ $PASSWORD =~ $Condition3 ]]; then                                   # when 3 is ok
-        echo -e "${RED}Password need's to containe "{$Condition1}"${ENDCOLOR} " # we return the error only 1
-        exit 1
-      else
-        echo -e "${RED}Password need's to containe "{$Condition1+$Condition3}"${ENDCOLOR} " # unless 3 is also missing we return 1+3
-        exit 1
-      fi
-    fi
-  else                                              # when the 1'st is ok we start again to check the other two
-    if [[ $PASSWORD =~ $Condition2 ]]; then         # nuber 2 is ok so
-      if [[ $PASSWORD =~ $Condition3 ]]; then       # we check if 3 is ok too is so the password is valid
-        echo -e "${GREEN}valid Password${ENDCOLOR}" #return the propper value and massage
-        exit 0
-      else
-        echo -e "${RED}Password need's to containe "{$Condition3}"${ENDCOLOR} " # else the only one missing is 3 so we return the propper error
-        exit 1
-      fi
-    else                                                                        # 1 is ok but 2 is not so we need to check 3 again
-      if [[ $PASSWORD =~ $Condition3 ]]; then                                   # 1 and 3 are ok
-        echo -e "${RED}Password need's to containe "{$Condition2}"${ENDCOLOR} " # return error of condition2
-        exit 1
-      else
-        echo -e "${RED}Password need's to containe "{$Condition2+$Condition3}"${ENDCOLOR} " # finally when only 1 is ok we return an error for the other two
-        exit 1
-      fi
-    fi
-  fi
 }
 
 PasswordValidator "$1"
